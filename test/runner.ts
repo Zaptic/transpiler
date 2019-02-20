686c4ac3aa73e065fe4c886004216321b7543597
Ben <ben@zaptic.com>
2019-02-20 15:43:41 +0000

    Add a test runner to be able to run some tests

---
+++ test/runner.ts
@@ -0,0 +1,34 @@
+import { assert } from 'chai' // tslint:disable-line
+import * as fs from 'fs'
+import * as path from 'path'
+import { module } from '../src/modules/jsonSchema'
+import { processFiles } from '../src/transpiler'
+
+// TODO make this friendlier to use
+const skip: string[] = []
+const only: string = ''
+
+describe('The Library', function() {
+    const featuresRootFolder = path.resolve('test')
+
+    const inputs = fs
+        .readdirSync(featuresRootFolder)
+        .filter(folder => !folder.endsWith('.ts'))
+        .map(folder => [
+            folder,
+            path.join(featuresRootFolder, folder, `${folder}.input.ts`),
+            path.join(featuresRootFolder, folder, `${folder}.expected.ts`),
+        ])
+
+    inputs.forEach(([featureName, inputFilePath, expectedFilePath]) => {
+        it(`${featureName}`, async function() {
+            if (skip.includes(featureName)) return console.log(`Skipping ${featureName}`)
+            if (only && featureName !== only) return console.log(`Skipping ${featureName}`)
+
+            const result = processFiles({ module, filePaths: [inputFilePath] })
+            const expected = (await import(expectedFilePath)).default
+
+            assert.deepEqual(result, expected)
+        })
+    })
+})
